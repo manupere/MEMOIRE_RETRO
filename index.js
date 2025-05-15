d7b23861e3b93b31ade9e766f09a28e2316f4235
Emmanuel <e.pereira1005@gmail.com>
2025-05-15 18:42:16 +0200

    fix: reparation dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require( "express");
 const pg = require( "pg");
-const dotenv =require("dotenv");
+ require("dotenv").config();
 
 // création serveur
 const app = express();
@@ -12,6 +12,7 @@ app.get("/",(req,res)=>{
 res.send("HELLO WORLD");
 });
 //ecoute serveur
+
 app.listen(3000,()=>{
     console.log('Serveur demarré sur port 3000');
 });
