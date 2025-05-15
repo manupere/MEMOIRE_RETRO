cb752be9de346cf6547512a2155cc0e86965234c
Emmanuel <e.pereira1005@gmail.com>
2025-05-15 18:54:25 +0200

    add: .env

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const app = express();
 app.set("engine view","ejs");
 app.set("views","./views") 
 
+const PORT = process.env.PORT;
+
 //route
 app.get("/",(req,res)=>{
 res.send("HELLO WORLD");
 });
 //ecoute serveur
 
-app.listen(3000,()=>{
-    console.log('Serveur demarré sur port 3000');
+app.listen(PORT,()=>{
+    // console.log('Serveur demarré sur port 3000',PORT);
+    console.log(`Serveur démarré sur le port ${PORT}`);
+    
 });
